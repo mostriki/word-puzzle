e386f0b01e0f10cfafc37c50bddae507dce9f2f6
Mike Holland and Riki Montgomery <mike@>
2017-08-15 11:38:27 -0700

    add initial html and scripts.

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("form#userPuzzle").submit(function(event) {
+    event.preventDefault();
+    var quote = $("#quote").val;
+    
+
+  });
+
+
+
+
+
+
+
+
+
+
+//   var string =
+//   var vowel = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+//
+//   var result = sring.replace(vowel);
+//
+// if (string === vowel) {
+//   .replace("-");
+//
+// }
